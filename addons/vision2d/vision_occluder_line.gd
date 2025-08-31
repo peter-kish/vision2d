f1be3a4f255b0b68a6ce76f3be49d126a7e7e08b
Peter Kis <pkish023@gmail.com>
2025-08-31 20:28:30 +0200

    Turn the whole thing into a plugin

--- addons/vision2d/vision_occluder_line.gd
+++ addons/vision2d/vision_occluder_line.gd
@@ -1,25 +1,27 @@
-extends Polygon2D
-class_name VisionOccluderPolygon
+@icon("res://addons/vision2d/icons/vision2d_vision_occluder_line.svg")
+extends Line2D
+class_name VisionOccluderLine
+
 
 const _VisionOccluder = preload("vision_occluder.gd")
 
 @export var observer: Node2D
-@export var shadow_color = Color.BLACK
+@export var color = Color.BLACK
 @export var size = 128.0
 @export var penetration = 32.0
 @export var max_segment_size = 16.0;
 
 
 func _ready() -> void:
-    if polygon.size() < 3:
+    if points.size() < 2:
         return
         
-    for i in range(polygon.size()):
+    for i in range(points.size() - 1):
         var occluder = _VisionOccluder.new()
-        occluder.start_point = polygon[i]
-        occluder.end_point = polygon[(i + 1) % polygon.size()]
+        occluder.start_point = points[i]
+        occluder.end_point = points[i + 1]
         occluder.observer = observer
-        occluder.color = shadow_color
+        occluder.color = color
         occluder.size = size
         occluder.penetration = penetration
         occluder.max_segment_size = max_segment_size
